7931d2946f210e7b33b60ee491fb07329bf04348
Hector Fernandez <hectorj@gmail.com>
2016-09-22 19:18:50 +0200

    Added counter to register visits to the web page

--- workers/dummy-sinatra/dummy.rb
+++ workers/dummy-sinatra/dummy.rb
@@ -1,29 +1,19 @@
-require 'redis'
 require 'sinatra'
-require 'yajl'
-require 'digest/sha1'
-
-set :url_set,    'urls'
-set :all_hits, 'all_hits'
-
-configure do
-  REDIS = Redis.new
-end
+require 'json'
+require 'redis'
 
 set :bind, '0.0.0.0'
 
-get '/' do
-  etag(Digest::SHA1.hexdigest(REDIS.get("#{settings.url_set}:#{settings.all_hits}").to_s))
-  result  = REDIS.sort(settings.url_set, :by => 'nosort', :get => ['#', "#{settings.url_set}:*"])
-  hash = Hash[result]
-  Yajl::Encoder.encode(hash) + "\n"
-  "You are talking to MACHINE: #{ENV["MACHINE_IP"]}".strip
+# configure redis
+$Redis = Redis.new(host: ENV["REDIS_PORT_6379_TCP_ADDR"], port: ENV["REDIS_PORT_6379_TCP_PORT"])
+
+def json(hash)
+  content_type :json
+  hash.to_json
 end
 
-post '/' do
-  url = params['url']
-  REDIS.incr("#{settings.url_set}:#{url}")
-  REDIS.sadd(settings.url_set, url)
-  REDIS.incr("#{settings.url_set}:#{settings.all_hits}")
-  ""
+get '/' do
+  count = $Redis.incr( "request_count" )
+  json :count => count
+  "You are talking to MACHINE: #{ENV["MACHINE_IP"]} --> Counter of visits: #{count}".strip
 end
